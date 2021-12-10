81a8d52756761fa64f1411723a575325f2251742
Dan Levy <dan.levy@Hotelengine.com>
2021-12-10 09:44:15 -0700

    WIP: Jest ESM still broken

--- src/jest.config.ts
+++ src/jest.config.ts
@@ -13,7 +13,7 @@ const config: Config.InitialOptions = {
       useESM: true,
     },
   },
-  testEnvironment: "jsdom",
+  testEnvironment: "node",
   transform: {
     "^.+\\.(t|j)sx?$": "ts-jest",
     // "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
