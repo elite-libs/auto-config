13eb2de5e9cbe042c129d67b4bb307847ce23459
Dan Levy <dan@danlevy.net>
2022-05-15 03:41:40 -0600

    Update & clean deps

--- src/easy-config.ts
+++ src/easy-config.ts
@@ -1,4 +1,4 @@
-import { mapValues } from 'lodash';
+import mapValues from 'lodash.mapvalues';
 import { getEnvAndArgs, stripDashesSlashes } from './utils';
 import type { ConfigInputsRaw } from './types';
 
